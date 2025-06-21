510cef1be1c570bb955f61211044a20d9175767f
mcbunkus <austenlebeau@gmail.com>
2025-06-21 15:15:22 -0500

    style updates cleaning

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,13 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders"
+import { glob } from "astro/loaders";
 
 const posts = defineCollection({
-    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/posts" }),
-    schema: z.object({
-        title: z.string(),
-        pubDate: z.coerce.date(),
-        topics: z.array(z.string())
-    })
-})
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/posts" }),
+  schema: z.object({
+    title: z.string().toLowerCase(),
+    pubDate: z.coerce.date(),
+    topics: z.array(z.string()),
+  }),
+});
 
 export const collections = { posts };
